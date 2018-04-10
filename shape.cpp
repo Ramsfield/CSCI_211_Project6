e1268c5161609a62d59a368ddd826ac00f1a5b4b
Ramsfield <mgriffith004@gmail.com>
2018-04-10 15:34:25 -0700

    Realized that shape.cpp was empty. It's not anymore

--- shape.cpp
+++ shape.cpp
@@ -2,3 +2,28 @@
 //Michael Griffith
 //Project 6
 #include"shape.h"
+
+//Wow
+//This place is desolate
+//its like empty, yo
+//Really needed this file
+//Gotta keep it though
+//Maybe I could have put a constuctor in here
+//Nahhhhh. Gonna load this bad boi up with comments.
+//That preprocessor directive though. That's the good stuff.
+
+
+//#include<iostream>
+//
+//int main()
+//{
+//std::cout << "Hello World!" << std::endl;
+//return 0;
+//}
+//
+//void jelloWorld(char * helloWorld)
+//{
+// std::cout << *helloWorld << std::endl;
+// if(*helloWorld!=d)
+// return jelloWorld(helloWorld+1);
+//}
